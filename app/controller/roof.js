0d74725a960d75787dc459f0f605b2f94f315294
BlackMass <shark_dong@163.com>
2018-04-29 00:06:30 +0800

    fix: 将排板子算法移动到后台

--- app/controller/roof.js
+++ app/controller/roof.js
@@ -66,7 +66,7 @@ class RoofController extends Controller {
     console.log('this is rate ===>>', req.rate)
     console.log('this is bacground params ===>>', bg.points)
     // 画出背景图片和其他图片
-    let rects = req.rects
+    let rects = resp.rects
     rects.push(bg)
     // 循环计算可以排放的板子
     console.log('this is 循环计算可以排放的板子 ===>>', totalHeight, spec.height)
@@ -122,7 +122,9 @@ class RoofController extends Controller {
         rects.push(rTriangle)
       }
       // 判断板子坐标是否与障碍物重叠，如果重叠，设置板子数组为不可渲染状态
-      rects.forEach((rect, index) => {
+      for (let rI = 0, length = rects.length; rI < length; rI++) {
+        // rects.forEach((rect, index) => {
+        let rect = rects[rI]
         if (rect.type && rect.type === 'bz') {
           let rectPoints = rect.points
           // 矩形的4个点
@@ -137,27 +139,39 @@ class RoofController extends Controller {
             let tl2 = ltrianglePoints[1]
             let tl3 = ltrianglePoints[2]
             // 矩形的4个边与三角形的3个边任意一个相交，则不渲染
-            if (this.segmentsIntr(p1, p2, tl1, tl2) || this.segmentsIntr(p1, p2, tl2, tl3) || this.segmentsIntr(p1, p2, tl1, tl3) ||
-              this.segmentsIntr(p2, p4, tl1, tl2) || this.segmentsIntr(p2, p4, tl2, tl3) || this.segmentsIntr(p2, p4, tl1, tl3) ||
-              this.segmentsIntr(p3, p4, tl1, tl2) || this.segmentsIntr(p3, p4, tl2, tl3) || this.segmentsIntr(p3, p4, tl1, tl3) ||
-              this.segmentsIntr(p1, p3, tl1, tl2) || this.segmentsIntr(p1, p3, tl2, tl3) || this.segmentsIntr(p1, p3, tl1, tl3)
-            ) {
+            // if (this.segmentsIntr(p1, p2, tl1, tl2) || this.segmentsIntr(p1, p2, tl2, tl3) || this.segmentsIntr(p1, p2, tl1, tl3) ||
+            //   this.segmentsIntr(p2, p4, tl1, tl2) || this.segmentsIntr(p2, p4, tl2, tl3) || this.segmentsIntr(p2, p4, tl1, tl3) ||
+            //   this.segmentsIntr(p3, p4, tl1, tl2) || this.segmentsIntr(p3, p4, tl2, tl3) || this.segmentsIntr(p3, p4, tl1, tl3) ||
+            //   this.segmentsIntr(p1, p3, tl1, tl2) || this.segmentsIntr(p1, p3, tl2, tl3) || this.segmentsIntr(p1, p3, tl1, tl3)
+            // ) {
+            //   // 是否渲染
+            //   rect.canDraw = false
+            //   resp.num -= 1
+            // }
+            let j1 = await this.jugeOverlay(ltrianglePoints, rectPoints)
+            if (j1) {
               // 是否渲染
               rect.canDraw = false
               resp.num -= 1
             }
 
             let rtrianglePoints = rTriangle.points
-            // 三角形的4个点
+            // 三角形的3个点
             let tr1 = rtrianglePoints[0]
             let tr2 = rtrianglePoints[1]
             let tr3 = rtrianglePoints[2]
             // 矩形的4个边与三角形的3个边任意一个相交，则不渲染
-            if (this.segmentsIntr(p1, p2, tr1, tr2) || this.segmentsIntr(p1, p2, tr2, tr3) || this.segmentsIntr(p1, p2, tr1, tr3) ||
-              this.segmentsIntr(p2, p4, tr1, tr2) || this.segmentsIntr(p2, p4, tr2, tr3) || this.segmentsIntr(p2, p4, tr1, tr3) ||
-              this.segmentsIntr(p3, p4, tr1, tr2) || this.segmentsIntr(p3, p4, tr2, tr3) || this.segmentsIntr(p3, p4, tr1, tr3) ||
-              this.segmentsIntr(p1, p3, tr1, tr2) || this.segmentsIntr(p1, p3, tr2, tr3) || this.segmentsIntr(p1, p3, tr1, tr3)
-            ) {
+            // if (this.segmentsIntr(p1, p2, tr1, tr2) || this.segmentsIntr(p1, p2, tr2, tr3) || this.segmentsIntr(p1, p2, tr1, tr3) ||
+            //   this.segmentsIntr(p2, p4, tr1, tr2) || this.segmentsIntr(p2, p4, tr2, tr3) || this.segmentsIntr(p2, p4, tr1, tr3) ||
+            //   this.segmentsIntr(p3, p4, tr1, tr2) || this.segmentsIntr(p3, p4, tr2, tr3) || this.segmentsIntr(p3, p4, tr1, tr3) ||
+            //   this.segmentsIntr(p1, p3, tr1, tr2) || this.segmentsIntr(p1, p3, tr2, tr3) || this.segmentsIntr(p1, p3, tr1, tr3)
+            // ) {
+            //   // 是否渲染
+            //   rect.canDraw = false
+            //   resp.num -= 1
+            // }
+            let j2 = await this.jugeOverlay(rtrianglePoints, rectPoints)
+            if (j2) {
               // 是否渲染
               rect.canDraw = false
               resp.num -= 1
@@ -169,18 +183,25 @@ class RoofController extends Controller {
             let t2 = trianglePoints[1]
             let t3 = trianglePoints[2]
             // 矩形的4个边与三角形的3个边任意一个相交，则不渲染
-            if (this.segmentsIntr(p1, p2, t1, t2) || this.segmentsIntr(p1, p2, t2, t3) || this.segmentsIntr(p1, p2, t1, t3) ||
-              this.segmentsIntr(p2, p4, t1, t2) || this.segmentsIntr(p2, p4, t2, t3) || this.segmentsIntr(p2, p4, t1, t3) ||
-              this.segmentsIntr(p3, p4, t1, t2) || this.segmentsIntr(p3, p4, t2, t3) || this.segmentsIntr(p3, p4, t1, t3) ||
-              this.segmentsIntr(p1, p3, t1, t2) || this.segmentsIntr(p1, p3, t2, t3) || this.segmentsIntr(p1, p3, t1, t3)
-            ) {
+            // if (this.segmentsIntr(p1, p2, t1, t2) || this.segmentsIntr(p1, p2, t2, t3) || this.segmentsIntr(p1, p2, t1, t3) ||
+            //   this.segmentsIntr(p2, p4, t1, t2) || this.segmentsIntr(p2, p4, t2, t3) || this.segmentsIntr(p2, p4, t1, t3) ||
+            //   this.segmentsIntr(p3, p4, t1, t2) || this.segmentsIntr(p3, p4, t2, t3) || this.segmentsIntr(p3, p4, t1, t3) ||
+            //   this.segmentsIntr(p1, p3, t1, t2) || this.segmentsIntr(p1, p3, t2, t3) || this.segmentsIntr(p1, p3, t1, t3)
+            // ) {
+            //   // 是否渲染
+            //   rect.canDraw = false
+            //   resp.num -= 1
+            // }
+            let j3 = await this.jugeOverlay(trianglePoints, rectPoints)
+            if (j3) {
               // 是否渲染
               rect.canDraw = false
               resp.num -= 1
             }
           }
         }
-      })
+      }
+      // )
     } else if (req.obstacle.postion !== 'none' && req.obstacle.type === 'R') {
       let rectObs = {
         'style': '#adadad',
@@ -197,7 +218,9 @@ class RoofController extends Controller {
       }
       rects.push(rectObs)
       // 判断板子坐标是否与障碍物重叠，如果重叠，设置板子数组为不可渲染状态
-      rects.forEach((rect, index) => {
+      for (let i = 0, length = rects.length; i < length; i++) {
+        let rect = rects[i]
+        // rects.forEach((rect, index) => {
         if (rect.type && rect.type === 'bz') {
           let rectPoints = rect.points
           // 矩形的4个点
@@ -213,11 +236,8 @@ class RoofController extends Controller {
           let r3 = rectObsPoints[3]
           // 矩形的4个边与矩形的4个边任意一个相交，则不渲染
           // 1. 相交肯定不渲染
-          if (this.segmentsIntr(p1, p2, r1, r2) || this.segmentsIntr(p1, p2, r2, r4) || this.segmentsIntr(p1, p2, r3, r4) || this.segmentsIntr(p1, p2, r1, r3) ||
-            this.segmentsIntr(p2, p4, r1, r2) || this.segmentsIntr(p2, p4, r2, r4) || this.segmentsIntr(p2, p4, r3, r4) || this.segmentsIntr(p2, p4, r1, r3) ||
-            this.segmentsIntr(p3, p4, r1, r2) || this.segmentsIntr(p3, p4, r2, r4) || this.segmentsIntr(p3, p4, r3, r4) || this.segmentsIntr(p3, p4, r1, r3) ||
-            this.segmentsIntr(p1, p3, r1, r2) || this.segmentsIntr(p1, p3, r2, r4) || this.segmentsIntr(p1, p3, r3, r4) || this.segmentsIntr(p1, p3, r1, r3)
-          ) {
+          let b = await this.checkRectAndRect(rectPoints, rectObsPoints)
+          if (b) {
             // 是否渲染
             rect.canDraw = false
             resp.num -= 1
@@ -245,11 +265,17 @@ class RoofController extends Controller {
             // 4. 相切
             if (rect.canDraw) {
               let tangentNum = 0
-              rectPoints.forEach((rectP) => {
-                if (this.checkPointInLine(rectP, r1, r2) || this.checkPointInLine(rectP, r2, r4) || this.checkPointInLine(rectP, r3, r4) || this.checkPointInLine(rectP, r1, r3)) {
+              for (let i = 0, length = rectPoints.length; i < length; i++ ) {
+              // .forEach((rectP) => {
+                let rectP = rectPoints[i]
+                let c1 = await this.checkPointInLine(rectP, r1, r2)
+                let c2 = await this.checkPointInLine(rectP, r2, r4)
+                let c3 = await this.checkPointInLine(rectP, r3, r4)
+                let c4 = await this.checkPointInLine(rectP, r1, r3)
+                if (c1 || c2 || c3 || c4) {
                   tangentNum++
                 }
-              })
+              }
               if (tangentNum === 4) {
                 rect.canDraw = false
                 resp.num -= 1
@@ -257,8 +283,42 @@ class RoofController extends Controller {
             }
           }
         }
-      })
+        // )
+      }
     }
+  }
+
+  async checkRectAndRect( rect1, rect2 ) {
+    // 矩形的4个点
+    let p1 = rect1[0]
+    let p2 = rect1[1]
+    let p4 = rect1[2]
+    let p3 = rect1[3]
+    // 矩形的4个点
+    let r1 = rect2[0]
+    let r2 = rect2[1]
+    let r4 = rect2[2]
+    let r3 = rect2[3]
+    let b1 = await this.intersect(p1, p2, r1, r2)
+    let b2 = await this.intersect(p1, p2, r2, r4)
+    let b3 = await this.intersect(p1, p2, r3, r4)
+    let b4 = await this.intersect(p1, p2, r1, r3)
+    let b5 = await this.intersect(p2, p4, r1, r2)
+    let b6 = await this.intersect(p2, p4, r2, r4)
+    let b7 = await this.intersect(p2, p4, r3, r4)
+    let b8 = await this.intersect(p2, p4, r1, r3)
+    let b9 = await this.intersect(p3, p4, r1, r2)
+    let b10 = await this.intersect(p3, p4, r2, r4)
+    let b11 = await this.intersect(p3, p4, r3, r4)
+    let b12 = await this.intersect(p3, p4, r1, r3)
+    let b13 = await this.intersect(p1, p3, r1, r2)
+    let b14 = await this.intersect(p1, p3, r2, r4)
+    let b15 = await this.intersect(p1, p3, r3, r4)
+    let b16 = await this.intersect(p1, p3, r1, r3)
+    if ( b1 || b2 || b3 || b4 || b5 || b6 || b7 || b8 || b9 || b10 || b11 || b12 || b13 || b14 || b15 || b16 ) {
+      return true
+    }
+    return false
   }
 
   /**
@@ -319,7 +379,7 @@ class RoofController extends Controller {
    *  @param width 宽度
    *  @param height 高度
    */
-  getTrianglePoints (startX, startY, width, height, req) {
+  async getTrianglePoints (startX, startY, width, height, req) {
     // 左下角坐标
     let p1 = {x: startX, y: startY}
     // 右下角坐标
@@ -333,7 +393,7 @@ class RoofController extends Controller {
    *  线段一坐标 （a点和b点）
    *  线段二坐标（c点和d点）
    */
-  segmentsIntr (a, b, c, d) {
+  async segmentsIntr (a, b, c, d) {
     /* 1 解线性方程组, 求线段交点.
      * 如果分母为0 则平行或共线, 不相交
      */
@@ -366,6 +426,143 @@ class RoofController extends Controller {
     return false
   }
 
+  // 判断两条线段是否相交
+  async intersect(aa, bb, cc, dd) {
+    if ( Math.max(aa.x, bb.x)< Math.min(cc.x, dd.x) ) {
+      return false;
+    }
+    if ( Math.max(aa.y, bb.y)< Math.min(cc.y, dd.y) ) {
+      return false;
+    }
+    if ( Math.max(cc.x, dd.x)< Math.min(aa.x, bb.x) ) {
+      return false;
+    }
+    if ( Math.max(cc.y, dd.y)< Math.min(aa.y, bb.y) ) {
+      return false;
+    }
+    let c1 = (await this.mult(cc, bb, aa))* (await this.mult(bb, dd, aa)) < 0
+    if (c1) {
+      return false;
+    }
+    let c2 = (await this.mult(aa, dd, cc))*(await this.mult(dd, bb, cc)) < 0
+    if (c2) {
+      return false;
+    }
+    return true;
+  }
+  async mult(a, b, c) {
+    return (a.x-c.x)*(b.y-c.y)-(b.x-c.x)*(a.y-c.y);
+  }
+
+  // 判断 三角形和四边形是否重叠
+  async jugeOverlay( triangle, rect) {
+    // 左下角
+    let t1 = triangle[0]
+    // 右下角
+    let t2 = triangle[1]
+    // 顶部
+    let t3 = triangle[2]
+
+    // 四边形
+    // 左上
+    let p1 = rect[0]
+    // 右上
+    let p2 = rect[1]
+    // 右下
+    let p4 = rect[2]
+    // 左下
+    let p3 = rect[3]
+
+    // 判断相交
+    let s1 = await this.intersect(p1, p2, t1, t2)
+    let s2 = await this.intersect(p1, p2, t2, t3)
+    let s3 = await this.intersect(p1, p2, t1, t3)
+    let s4 = await this.intersect(p2, p4, t1, t2)
+    let s5 = await this.intersect(p2, p4, t2, t3)
+    let s6 = await this.intersect(p2, p4, t1, t3)
+    let s7 = await this.intersect(p3, p4, t1, t2)
+    let s8 = await this.intersect(p3, p4, t2, t3)
+    let s9 = await this.intersect(p3, p4, t1, t3)
+    let s10 = await this.intersect(p1, p3, t1, t2)
+    let s11 = await this.intersect(p1, p3, t2, t3)
+    let s12 = await this.intersect(p1, p3, t1, t3)
+    if (s1 || s2 || s3 || s4 || s5 || s6 || s7 || s8 || s9 || s10 || s11 || s12) {
+      return true
+    }
+    // 四边形包含三角形 顶点与其中另一个点，在四边形之内
+    let rt = await this.checkTriangleInRect(triangle, rect)
+    if (rt) {
+      return true
+    }
+
+    // 三角形包含四边形
+    let tr = await this.checkLineInTriangle(p1, p2, triangle)
+    if (tr) {
+      return true
+    }
+
+    return false
+
+  }
+
+  // 判断三角形斜边是否在四边形之内
+  async checkTriangleInRect(triangle, rect) {
+    // 顶点
+    let t3 = triangle[2]
+    let t1 = triangle[0]
+    let t2 = triangle[1]
+
+    return (await this.checkLineInRect(t3, t1, rect) || await this.checkLineInRect(t3, t2, rect))
+  }
+
+  // 判断直线段是否在四边形之内
+  async checkLineInRect(a, b, rect) {
+    return await this.checkPointInRect(a, rect) && await this.checkPointInRect(b, rect)
+  }
+
+  // 判断点在四边形之内(包含边上)
+  async checkPointInRect(pos, rect) {
+    let p1 = rect[0]
+    let p3 = rect[2]
+    return (pos.x >= p1.x && pos.x <= p3.x) && (pos.y >= p1.y && pos.y <= p3.y)
+  }
+
+  // 判断直线段在三角形之内
+  async checkLineInTriangle(a, b, triangle) {
+    return (await this.checkPointInTriangle(a, triangle) && await this.checkPointInTriangle(b, triangle))
+  }
+
+  // 判断点在三角形之内
+  async checkPointInTriangle(pos, triangle) {
+    let x = pos.x;
+    let y = pos.z;
+    let v0 = triangle[0]
+    let v1 = triangle[1]
+    let v2 = triangle[2]
+
+    let v0x = v0.x;
+    let v0y = v0.z;
+
+    let v1x = v1.x;
+    let v1y = v1.z;
+
+    let v2x = v2.x;
+    let v2y = v2.z;
+
+    let t = await this.triangleArea(v0x, v0y, v1x, v1y, v2x, v2y);
+    let a = await this.triangleArea(v0x, v0y, v1x, v1y, x, y) + await this.triangleArea(v0x, v0y, x, y, v2x, v2y) + await this.triangleArea(x, y, v1x, v1y, v2x, v2y);
+
+    if (Math.abs(t - a) <= 0.01) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  async triangleArea (t1x, t1y, t2x, t2y, t3x, t3y) {
+    return Math.abs((t1x * t2y + t2x * t3y + t3x * t1y - t2x * t1y - t3x * t2y - t1x * t3y) / 2);
+  }
+
 }
 
-module.export = RoofController
+module.exports = RoofController

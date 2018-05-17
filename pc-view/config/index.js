2a9a281d727ed829d85813e3f8a5a32285eb3f1b
dengkang <dengkangv3@sina.cn>
2018-05-17 18:40:51 +0800

    fix:页面打包

--- pc-view/config/index.js
+++ pc-view/config/index.js
@@ -56,7 +56,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../../app/public'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/front-api/public/',
 
     /**
      * Source Maps
